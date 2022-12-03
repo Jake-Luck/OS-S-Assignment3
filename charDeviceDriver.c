c7a3eeab2f2fa1a149749bf6f8a333281d029791
jake-luck <jhhluck@gmail.com>
2022-12-03 20:32:57 +0000

    Initialise new folder

---
+++ charDeviceDriver.c
@@ -0,0 +1,134 @@
+#include <linux/kernel.h> // kernel stuff
+#include <linux/module.h> // module stuff
+#include <linux/slab.h> // kmalloc
+#include <linux/fs.h> // file stuff
+#include <linux/types.h> // ssize etc
+#include <linux/uaccess.h> //copy from user
+#include "charDeviceDriver.h"
+
+MODULE_LICENSE("GPL");
+
+static struct file_operations fops = {
+	.read = device_read,
+	.write = device_write,
+	.open = device_open,
+	.release = device_release
+};
+
+#define DEVICE_NAME "chardev"
+#define MAX_MESSAGES 1000
+#define MAX_MESSAGE_BYTES 4096
+DEFINE_MUTEX(lock);
+
+static int Major;
+static int Device_Open = 0;
+static char* messageQueue;
+static int head = 0;
+static int queueLength = 0;
+
+int init_module(void) {
+    Major = register_chrdev(0, DEVICE_NAME, &fops);
+	if (Major < 0) {
+	  printk(KERN_ALERT "Registering char device failed with %d\n", Major);
+	  return Major;
+	}
+    if ((messageQueue = kmalloc(MAX_MESSAGES * sizeof(char), GFP_KERNEL)) == NULL) {
+        printk(KERN_INFO "Memory allocation failed");
+        return -EFAULT;
+    }
+
+	printk(KERN_INFO "Major number %d. To talk to\n", Major);
+	printk(KERN_INFO "the driver, create a dev file with\n");
+	printk(KERN_INFO "'mknod /dev/%s c %d 0'.\n", DEVICE_NAME, Major);
+	printk(KERN_INFO "Remove the device file and module when done.\n");
+
+	return 0;
+}
+
+void cleanup_module(void)
+{
+	/*  Unregister the device */
+	unregister_chrdev(Major, DEVICE_NAME);
+    for (head = 0; head < MAX_MESSAGES; head++) {
+        kfree(messageQueue[head]);
+    }
+    kfree(messageQueue);
+}
+
+static int device_open(struct inode *inode, struct file *file) {
+    mutex_lock(&lock);
+    if (Device_Open == 1) {
+        mutex_unlock(&lock);
+        return -EBUSY;
+    }
+    Device_Open = 1;
+    mutex_unlock(&lock);
+    
+    printk(KERN_INFO "Device has been opened\n");
+    try_module_get(THIS_MODULE);
+    return 0;
+}
+
+static int device_release(struct inode *inode, struct file *file) {
+    mutex_lock(&lock);
+    Device_Open = 0;
+    mutex_unlock(&lock);
+
+    printk(KERN_INFO "Device has been closed\n");
+    module_put(THIS_MODULE);
+    return 0;
+}
+
+static ssize_t device_read(struct file *filp, char *buffer, size_t bufferLength, loff_t *offset) {
+    mutex_lock(&lock);
+    printk(KERN_INFO "Test: Locked\n");
+    if(queueLength == 0) {
+        mutex_unlock(&lock);
+        printk(KERN_INFO "No messages to read\n");
+        return -EAGAIN;
+    }
+    printk(KERN_INFO "Test: Queue not empty\n");
+    if (copy_to_user(buffer, messageQueue + head, bufferLength) > 0) {
+        mutex_unlock(&lock);
+        printk(KERN_INFO "Copy went wrong\n");
+        return -EFAULT;
+    }
+    printk(KERN_INFO "Test: Message read\n");
+    kfree(messageQueue[head]);
+    printk(KERN_INFO "First queueLength = %d", queueLength);
+    queueLength--;
+    printk(KERN_INFO "Second queueLength = %d", queueLength);
+    mutex_unlock(&lock);
+    return bufferLength;
+}
+
+static ssize_t device_write(struct file *filp, const char *buffer, size_t bufferLength, loff_t *offset) {
+    char *messageWritten;
+    int messageLength;
+    messageLength = bufferLength + sizeof(char);
+    if (messageLength > MAX_MESSAGE_BYTES) {
+        printk(KERN_INFO "Message too long\n");
+        return -EINVAL;
+    }
+    if((messageWritten = kmalloc(messageLength, GFP_KERNEL)) == NULL) {
+        printk(KERN_INFO "Memory allocation failed\n");
+        return -EFAULT;
+    }
+    mutex_lock(&lock);
+    if (queueLength >= 1000) {
+        mutex_unlock(&lock);
+        printk(KERN_INFO "Message queue full\n");
+        return -EBUSY;
+    }
+    if(copy_from_user(messageWritten, buffer, bufferLength) > 0) {
+        mutex_unlock(&lock);
+        printk(KERN_INFO "Copy went wrong\n");
+        return -EFAULT;
+    }
+    messageWritten[bufferLength] = '\0';
+    *(messageQueue + ((head + queueLength) % MAX_MESSAGES)) = *messageWritten;
+    kfree(messageWritten);
+    queueLength++;
+    mutex_unlock(&lock);
+    return bufferLength;
+}
