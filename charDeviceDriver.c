dd4ca41dc17c06730a17673c49b5b705d6827592
jake-luck <jhhluck@gmail.com>
2022-12-06 19:04:36 +0000

    Works with only one message in list

--- charDeviceDriver.c
+++ charDeviceDriver.c
@@ -22,7 +22,7 @@ DEFINE_MUTEX(lock);
 
 static int Major;
 static int Device_Open = 0;
-static char* messageQueue;
+static char **messageQueue;
 static int head = 0;
 static int queueLength = 0;
 
@@ -84,8 +84,8 @@ static ssize_t device_read(struct file *filp, char *buffer, size_t bufferLength,
             printk(KERN_ALERT "No messages to read\n");
             return -EAGAIN;
         }
-        if(strlen(messageQueue+head) + 1 < bufferLength) bufferLength = strlen(messageQueue+head) + 1;
-        if (copy_to_user(buffer, messageQueue + head, bufferLength) > 0) {
+        if(strlen(*(messageQueue+head)) + 1 < bufferLength) bufferLength = strlen(*(messageQueue+head)) + 1;
+        if (copy_to_user(buffer, *(messageQueue + head), bufferLength) > 0) {
             mutex_unlock(&lock);
             printk(KERN_ALERT "Copy went wrong\n");
             return -EFAULT;
@@ -99,14 +99,13 @@ static ssize_t device_read(struct file *filp, char *buffer, size_t bufferLength,
 }
 
 static ssize_t device_write(struct file *filp, const char *buffer, size_t bufferLength, loff_t *offset) {
-    char *messageWritten;
     int messageLength;
     messageLength = bufferLength + sizeof(char);
     if (messageLength > MAX_MESSAGE_BYTES) {
         printk(KERN_ALERT "Message too long\n");
         return -EINVAL;
     }
-    if((messageWritten = kmalloc(messageLength, GFP_KERNEL)) == NULL) {
+    if((*(messageQueue + ((head + queueLength) % MAX_MESSAGES)) = kmalloc(messageLength, GFP_KERNEL)) == NULL) {
         printk(KERN_ALERT "Memory allocation failed\n");
         return -EFAULT;
     }
@@ -117,14 +116,12 @@ static ssize_t device_write(struct file *filp, const char *buffer, size_t buffer
             printk(KERN_ALERT "Message queue full\n");
             return -EBUSY;
         }
-        if(copy_from_user(messageWritten, buffer, bufferLength) > 0) {
+        if(copy_from_user(*(messageQueue + ((head + queueLength) % MAX_MESSAGES)), buffer, bufferLength) > 0) {
             mutex_unlock(&lock);
             printk(KERN_ALERT "Copy went wrong\n");
             return -EFAULT;
         }
-        messageWritten[bufferLength] = '\0';
-        *(messageQueue + ((head + queueLength) % MAX_MESSAGES)) = *messageWritten;
-        kfree(messageWritten);
+        *(*(messageQueue + ((head + queueLength) % MAX_MESSAGES)) + bufferLength) = '\0';
         queueLength++;
     mutex_unlock(&lock);
 
